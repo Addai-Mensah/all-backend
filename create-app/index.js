d7ca8d19d0624a1d3889e3755bb1728af63b9c31
Addai-Mensah <mensahaddaai@gmail.com>
2023-03-05 21:10:06 +0000

    created delete

--- create-app/index.js
+++ create-app/index.js
@@ -5,66 +5,89 @@ const app = express();
 const {json} = require("express");
 app.use(express.json());
 
-mongoose.connect()
+mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true')
+.then (() => console.log("successfully connected to mongodb"))
+.catch(() => console.error("could not connect to mongodb"))
 
 
 
 
-const movie = [
-    {id: 1, genres:"action"},
-    {id: 2, genres: "anime"},
-    {id: 3, genres: "comedy"},
-    {id: 4, genres: "drama"},
-    {id: 5, genres: "sci-fi"},
-]
+// const movie = [
+//     {id: 1, genres:"action"},
+//     {id: 2, genres: "anime"},
+//     {id: 3, genres: "comedy"},
+//     {id: 4, genres: "drama"},
+//     {id: 5, genres: "sci-fi"},
+// ]
+
+const movieSchema = new mongoose.Schema({
+    genres:{
+        type:String,
+        required: true,
+        maxlength: 40,
+        minlength:4
+    }
+});
+
+ const Movie = mongoose.model("Movie", movieSchema);
 
 
-app.get("/api/movies",(req,res) =>{
+app.get("/", async (req,res) =>{
+
+    const movie = await  Movie.find()
+    .sort({genres:1})
+    
     res.send(movie)
 })
 
+app.get("/:id", async (req,res) =>{
+    const movieGenres = await Movie.findById(req.params.id)
 
-app.get("/api/movies/:id", (req,res) =>{
-    movieGenre = movie.find( m => m.id === parseInt(req.params.id))
-    if(!movieGenre) res.status(404).send("Course with this ID cannot be found")
-    res.send(movieGenre)
+    if(!movieGenres) return res.status(404).send("Movie with this ID cannot be found")
+
+    res.send(movieGenres)
 })
 
 
-app.post("/api/movies/",(req,res) =>{
+app.post("/", async (req,res) =>{
     const {error} = validateMovie(req.body)
     if (error)
        return res.status(400).send(error.details[0].message)
-    
-       const createMovie = {
-        id: movie.length +1,
-        genres: req.body.genres
-       }
-       movie.push(createMovie)
-       res.send(createMovie)
+
+       let createMovie = new Movie({
+        genres:req.body.genres,
+      
+       })
+      createMovie = await createMovie.save()
+       res.send(createMovie);
 })
 
-app.put("/api/movies/:id", (req,res) =>{
-    movieGenre = movie.find( m => m.id === parseInt(req.params.id))
-    if(!movieGenre) res.status(404).send("Movie with this ID cannot be found")
+app.put("/:id", async (req,res) =>{
 
     const {error} = validateMovie(req.body)
     if (error) res.status(404).send(error.details[0].message)
 
-    movieGenre.genres = req.body.genres
+    const  movieGenre = await Movie.findByIdAndUpdate(req.params.id, {
+    $set:{
+        genres: req.body.genres
+    }
+  },{new:true})
+    if(!movieGenre) res.status(404).send("Movie with this ID cannot be found")
+
     res.send(movieGenre)
 })
 
-app.delete("/api/movies/:id",(req,res) =>{
-    movieGenre = movie.find( m => m.id === parseInt(req.params.id))
-    if(!movieGenre) res.status(404).send("Movie with this ID cannot be found")
-
+app.delete("/:id", async (req,res) =>{
     const {error} = validateMovie(req.body)
     if (error) res.status(404).send(error.details[0].message)
 
-    const index = movie.indexOf(movieGenre);
-    movie.splice(index,1);
-    res.send(movieGenre);
+    const movieGenre = await Movie.findByIdAndRemove(req.params.id)
+    if(!movieGenre) res.status(404).send("Movie with this ID cannot be found")
+
+    res.send(movieGenre)
+   
+
+    
 })
 
 const port = process.env.PORT||3000
